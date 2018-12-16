bf1c3324ebb2baf37d4dbcc9f58e22dbdafa2869
Calvin Cheung <calvchum@gmail.com>
2018-12-16 15:06:46 +1100

    added firebase v5.7.0 to dependencies

--- src/base.js
+++ src/base.js
@@ -1,1 +1,9 @@
 import Rebase from 're-base';
+
+const base = Rebase.createclass({
+    apiKey: "AIzaSyBewNva1QfD1NCG5ekHFT7dOva8K8EBwZ8",
+    authDomain: "stacked-8b627.firebaseapp.com",
+    databaseURL: "https://stacked-8b627.firebaseio.com",
+});
+
+export default base;
