767ad2a668740c7d1986fa9252326b79ee54b0c2
Calvin Cheung <calvchum@gmail.com>
2019-01-02 14:19:14 +1100

    renderList is now returning the values from data state

---
+++ src/components/ListHerbs.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+class ListHerbs extends React.Component {
+
+  renderList = key => {
+    const dataItems = this.props.data[key].items
+    
+    return (
+      <ul key={key}>
+        {dataItems.map((e, index)=>{
+          return <li key={index}>{e.price} - {e.title}</li>          
+        })}
+      </ul>
+    )
+    
+
+    // return(
+    //   <p>hi</p>)
+  }
+
+  render() {
+    const dataId = Object.keys(this.props.data);
+    return (
+      <div>
+        <ul>
+          {dataId.map(this.renderList)}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default ListHerbs;
